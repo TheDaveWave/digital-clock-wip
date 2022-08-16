a0bb8446ae9e2eb6d5fbcdd891c6706f5a0ff44d
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-16 16:46:19 -0500

    found interesting date function

--- digital-clock.js
+++ digital-clock.js
@@ -1,10 +1,3 @@
 let d = new Date();
-console.log(d.getHours());
-console.log(d.getMinutes());
-console.log(d.getSeconds());
 
-let h = d.getHours();
-let m = d.getMinutes();
-let s = d.getSeconds();
-
-console.log('The time is ' + h + ':' + m + ':' + s);
+console.log(d.toLocaleTimeString());
