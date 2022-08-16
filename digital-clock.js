f78763207df4764757aa4bb4b7a2c8aecf4eac46
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-16 16:41:29 -0500

    log out current time

--- digital-clock.js
+++ digital-clock.js
@@ -1,2 +1,10 @@
 let d = new Date();
-d.getTime();
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+
+let h = d.getHours();
+let m = d.getMinutes();
+let s = d.getSeconds();
+
+console.log('The time is ' + h + ':' + m + ':' + s);
