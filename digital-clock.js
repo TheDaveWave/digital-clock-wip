4f25e9d68824006ea3e5d0cce4a53097d6a8d3ae
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-16 17:27:36 -0500

    trying to manipulate html using js

--- digital-clock.js
+++ digital-clock.js
@@ -1,5 +1,10 @@
 
-function myTime() {
-    let d = new Date();
-    document.getElementsByClassName("digital-display").innerHTML = d.toLocaleTimeString();
-}
+// function myTime() {
+//     const d = new Date();
+//     return document.getElementsByClassName("digital-display").innerHTML = d.toLocaleTimeString();
+// }
+
+// window.onload = myTime();
+
+const display = document.getElementsByClassName("digital-display");
+display.innerHTML = "Hello World"
