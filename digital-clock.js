705ebd2138a9dc707dfed0de4fa2b8b4837311be
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-16 19:25:17 -0500

    trying to make btn turn of and off the interval

--- digital-clock.js
+++ digital-clock.js
@@ -1,8 +1,46 @@
 window.onload = function() {
-    setInterval(myTime,1000);
+    // document.getElementById("btn-on").addEventListener('click', startClock);
+    btnClick();
 }
+
+// function startedClock() {
+//     setInterval(myTime,1000);
+// }
 
 function myTime() {
     const d = new Date();
     document.getElementById("digital-display").innerHTML = d.toLocaleTimeString();
+}
+
+let myTimer;
+
+function startClock() {
+   myTimer = setInterval(myTime, 1000);
+   return myTimer;
+}
+
+//fix this!
+function stopClock() {
+    window.clearInterval(myTimer);
+    return myTimer;
+}
+
+function btnClick() {
+    let e1 = document.getElementById('btn-on');
+    // let e2 = document.getElementById('btn-off');
+    // let btnClicked = false;
+    if(e1) {
+        e1.addEventListener('click',startClock);
+        // btnClicked = true;
+    } 
+    // if (btnClicked) {
+    //     e2.addEventListener('click',stopClock);
+    // }
+}
+
+function btnOffClick() {
+    let e = document.getElementById('btn-off');
+    if(e) {
+        e.addEventListener('click',stopClock);
+    }
 }
